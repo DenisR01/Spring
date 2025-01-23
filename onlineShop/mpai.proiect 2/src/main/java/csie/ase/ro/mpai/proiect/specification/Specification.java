2da6565e365c3100597b6e58adc8c13f2fd6abdd
Denis <77248226+DenisR01@users.noreply.github.com>
2025-01-23 12:32:25 +0200

    Delete onlineShop/mpai.proiect 2 directory

--- onlineShop/mpai.proiect 2/src/main/java/csie/ase/ro/mpai/proiect/specification/Specification.java
+++
@@ -1,5 +0,0 @@
-package csie.ase.ro.mpai.proiect.specification;
-
-public interface Specification<T> {
-    boolean isSatisfiedBy(T item);
-}
--- onlineShop/mpai.proiect 2/src/main/java/csie/ase/ro/mpai/proiect/visitor/Visitor.java
+++
@@ -1,7 +0,0 @@
-package csie.ase.ro.mpai.proiect.visitor;
-
-import csie.ase.ro.mpai.proiect.response.ExamResponse;
-
-public interface Visitor {
-    String visitExam(ExamResponse exam);
-}
